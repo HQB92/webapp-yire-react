76feb271c308b2f96ca97137022f7789f7c254ec
HQB92 <76698088+HQB92@users.noreply.github.com>
2021-09-30 13:07:57 -0300

    Final lista alumno
    Final en uso y sin errores

--- src/components/presensiales.js
+++ src/components/presensiales.js
@@ -1,56 +1,60 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { Table, Container, Row, Col, Button} from 'react-bootstrap';
-var valor=0;
-const Presenciales= () =>{
 
+const Presenciales= () =>{
+    const [consulta, setConsulta] =useState({
+        curso:'PREKINDER',
+        mes:'10',
+        mostar:'1',
+    });
+    const [mesBBDD, setMesBBDD] = useState([]);
     const [alumnos, setAlumnos] = useState([]);
+    const alumnoCurso = async(setAlumnos)=>{
+        setAlumnos([]);
+        setMesBBDD([]);
+        const data = await fetch(`https://portal.yireliceo.com/API/obtener_alumnos_curso.php?consulta=${consulta.curso}`)
+        const datoalumnos = await data.json();
+        setAlumnos(datoalumnos);
+        setMesBBDD(datoalumnos[0]);
+    }
+
     const alumnoSi = async(setAlumnos)=>{
         setAlumnos([]);
+        setMesBBDD([]);
         const data = await fetch(`https://portal.yireliceo.com/API/obtener_alumnos_curso_si.php?consulta=${consulta.curso}`)
         const datoalumnos = await data.json();
         setAlumnos(datoalumnos);
-        console.log(alumnos)
+        setMesBBDD(datoalumnos[0]);
     }
-    const alumnoCurso = async(setAlumnos)=>{
-        setAlumnos([]);
-        const data = await fetch(`https://portal.yireliceo.com/API/obtener_alumnos_curso.php?consulta=${consulta.curso}`)
-        const datoalumnos = await data.json();
-        
-        setAlumnos(datoalumnos);
-        console.log(alumnos)
-    }
-    const [consulta, setConsulta] =useState({
-        curso:'',
-        mes:''
-    });
     const handleChange =  e =>{
         setConsulta({
             ...consulta,
             [e.target.name]: e.target.value
         });
     }
-    const handleChangeMostrar =  e =>{
-        valor = e.target.value
-    }
+    useEffect(() => {
+        buscarDatos()
+    }, [consulta]);
+
     const buscarDatos = ()=>{
-        if (valor === "1") {
+        if (consulta.mostar === "1") {
             const aux = alumnoCurso(setAlumnos)
         }
-        if (valor === "2") {
+        if (consulta.mostar === "2") {
             const aux = alumnoSi(setAlumnos)
         }
     }
     return(
+        mesBBDD.mes === consulta.mes?
         <Container fluid="md" className="justify-content-md-center barra-login">
             <Row className="justify-content-md-center">
                 <Col md="auto"> <h2>Asistencia Presencial a Clase Octubre</h2></Col>
             </Row>
-            <Row>
-                {/*<Col md="3">
-                <h4>Mes</h4>
-                    <select name="mes" onChange={handleChange}>
-                        <option></option>
-                        <option value="1">Enero</option>
+            <Row className="text-center">
+                <Col md="4" className="text-center">
+                    <h4>Mes</h4>
+                    <select className="form-select text-center"  name="mes" onChange={handleChange}>
+                        {/*<option value="1">Enero</option>
                         <option value="2">Febrero</option>
                         <option value="3">Marzo</option>
                         <option value="4">Abril</option>
@@ -58,16 +62,15 @@ const Presenciales= () =>{
                         <option value="6">Junio</option>
                         <option value="7">Julio</option>
                         <option value="8">Agosto</option>
-                        <option value="9">Septiembre</option>
+                        <option value="9">Septiembre</option>*/}
                         <option value="10">Octubre</option>
                         <option value="11">Noviembre</option>
                         <option value="12">Diciembre</option>
                     </select>
-                </Col>*/}
-                <Col md="4">
+                </Col>
+                <Col md="4" >
                     <h4>Curso</h4>
-                    <select name="curso" onChange={handleChange}>
-                        <option></option>
+                    <select className="form-select text-center"  name="curso" onChange={handleChange}>
                         <option>PREKINDER</option>
                         <option>KINDER</option>
                         <option>1 BASICO</option>
@@ -88,14 +91,10 @@ const Presenciales= () =>{
                 </Col>
                 <Col md="4">
                     <h4>Mostrar</h4>
-                    <select name="mostar" onChange={handleChangeMostrar}>
-                        <option></option>
+                    <select className="form-select text-center"  aria-label="Default select example" name="mostar" onChange={handleChange}>
                         <option value="1">Todos</option>
                         <option value="2">Solo si</option>
                     </select>
-                </Col>
-                <Col md="4">
-                    <Button className="boton-filtrar" onClick={buscarDatos}>  Filtrar</Button>
                 </Col>
             </Row>
             <Row> <Col> </Col></Row>
@@ -121,6 +120,63 @@ const Presenciales= () =>{
                     </tbody>
                 </Table>
             </Row>
+        </Container>
+        :
+        <Container fluid="md" className="justify-content-md-center barra-login">
+            <Row className="justify-content-md-center">
+                <Col md="auto"> <h2>Asistencia Presencial a Clase Octubre</h2></Col>
+            </Row>
+            <Row className="text-center">
+                <Col md="4" className="text-center">
+                    <h4>Mes</h4>
+                    <select className="form-select selectpicker text-center"  name="mes" onChange={handleChange}>
+                     {/*<option value="1">Enero</option>
+                        <option value="2">Febrero</option>
+                        <option value="3">Marzo</option>
+                        <option value="4">Abril</option>
+                        <option value="5">Mayo</option>
+                        <option value="6">Junio</option>
+                        <option value="7">Julio</option>
+                        <option value="8">Agosto</option>
+                        <option value="9">Septiembre</option>*/}
+                        <option value="10">Octubre</option>
+                        <option value="11">Noviembre</option>
+                        <option value="12">Diciembre</option>
+                    </select>
+                </Col>
+                <Col md="4" >
+                    <h4>Curso</h4>
+                    <select className="form-select text-center"  name="curso" onChange={handleChange}>
+                        <option>PREKINDER</option>
+                        <option>KINDER</option>
+                        <option>1 BASICO</option>
+                        <option>2 BASICO</option>
+                        <option>3 BASICO</option>
+                        <option>4 BASICO</option>
+                        <option>5 BASICO</option>
+                        <option>6 BASICO</option>
+                        <option>7 BASICO</option>
+                        <option>8 BASICO</option>
+                        <option>1 MEDIO</option>
+                        <option>2 MEDIO</option>
+                        <option>3 MEDIO A TP</option>
+                        <option>3 MEDIO B HC</option>
+                        <option>4 MEDIO A TP</option>
+                        <option>4 MEDIO B HC</option>
+                    </select>
+                </Col>
+                <Col md="4">
+                    <h4>Mostrar</h4>
+                    <select className="form-select text-center"  aria-label="Default select example" name="mostar" onChange={handleChange}>
+                        <option value="1">Todos</option>
+                        <option value="2">Solo si</option>
+                    </select>
+                </Col>
+            </Row>
+            <Row className="text-center">
+                <Col><h1>Mes sin registros</h1></Col>
+            </Row>
+
         </Container>
     )
 }
